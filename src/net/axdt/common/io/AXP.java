0d66dfcb2dde0238847ce4a7a62bb499a1ab946c
lucasredsn0w <lucasredsn0w@hotmail.com>
2014-03-30 18:50:38 +0800

    Initial Commit.

---
+++ src/net/axdt/common/io/AXP.java
@@ -0,0 +1,69 @@
+package net.axdt.common.io;
+import java.io.*;
+
+import net.axdt.exception.*;
+public class AXP {
+	public static final int READ = 0;
+	public static final int WRITE = 1;
+	public FileReader reader;
+	public FileWriter writer;
+	public File f;
+	public byte[] linefeed = "\n".getBytes();
+	public byte[] lk = "{".getBytes();
+	public byte[] rk = "}".getBytes();
+	int avil[] = null;
+	public AXP(String filename,int openmode) throws AXInvaildArgumentException, AXInvaildAXPFileException{
+    	if(openmode==0){
+    		f = new File(filename);
+    		try {
+				reader = new FileReader(f);
+			} catch (FileNotFoundException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+    		for(int i=0;i<=10;i++){
+    			try {
+					avil[i]=reader.read();
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+    		}
+    		String check = avil.toString();
+    		if(check.equals("<AXP Begin>")){
+    			//do something
+    		}
+    		else{
+    			throw new AXInvaildAXPFileException("Invaild AXP Header!");
+    		}
+    	}
+    	else if(openmode==1){
+    		f = new File(filename);
+    		try {
+				writer = new FileWriter(f);
+				reader = new FileReader(f);
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+    		for(int i=0;i<=10;i++){
+    			try {
+					avil[i]=reader.read();
+				} catch (IOException e) {
+					// TODO 自动生成的 catch 块
+					e.printStackTrace();
+				}
+    		}
+    		String check = avil.toString();
+    		if(check.equals("<AXP Begin>")){
+    			
+    		}
+    		else{
+    			
+    		}
+    	}
+    	else{
+    		throw new AXInvaildArgumentException("Openmode is invaild");
+    	}
+    }
+}
---
+++ src/net/axdt/exception/AXInvaildAXPFileException.java
@@ -0,0 +1,24 @@
+package net.axdt.exception;
+
+public class AXInvaildAXPFileException extends Exception {
+
+	public AXInvaildAXPFileException() {
+		// TODO 自动生成的构造函数存根
+	}
+
+	public AXInvaildAXPFileException(String message) {
+		super(message);
+		// TODO 自动生成的构造函数存根
+	}
+
+	public AXInvaildAXPFileException(Throwable cause) {
+		super(cause);
+		// TODO 自动生成的构造函数存根
+	}
+
+	public AXInvaildAXPFileException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO 自动生成的构造函数存根
+	}
+
+}
